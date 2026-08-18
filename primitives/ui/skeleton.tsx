068b5eeb8f1ce419bda5cc666d4b4dc0f3e7bd6c
Pol Gubau Amores <gubaupol@gmail.com>
2026-08-19 00:12:36 +0200

    feat: add verifactuWarnings function and display AEAT warnings in InvoiceDetailPage

--- primitives/ui/skeleton.tsx
+++ primitives/ui/skeleton.tsx
@@ -1,6 +1,1 @@
-﻿import type { HTMLAttributes } from "react";
-import { cn } from "../lib/utils";
-
-export function Skeleton({ className, ...props }: HTMLAttributes<HTMLDivElement>) {
-  return <div className={cn("skeleton-shimmer rounded-md", className)} {...props} />;
-}
+﻿export { Skeleton } from "@doscientos/ui";
