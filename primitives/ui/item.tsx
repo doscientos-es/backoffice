ff5976812815a0211d20882890c1f3dd270ffded
Pol Gubau Amores <gubaupol@gmail.com>
2026-08-31 21:44:55 +0200

    feat: update imports to use centralized Separator component in Field and Item

--- primitives/ui/item.tsx
+++ primitives/ui/item.tsx
@@ -1,9 +1,9 @@
 ﻿import { cva, type VariantProps } from 'class-variance-authority'
 import { Slot } from 'radix-ui'
 import type * as React from 'react'
+import { Separator } from '@doscientos/ui'
 
 import { cn } from '../lib/utils'
-import { Separator } from './separator'
 
 function ItemGroup({ className, ...props }: React.ComponentProps<'div'>) {
   return (
